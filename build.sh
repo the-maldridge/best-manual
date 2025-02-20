903cddb66802b8f0c826d609a3f9d0cc1f92ae3e
Michael Aldridge <aldridge.mac@gmail.com>
2025-02-19 22:37:00 -0600

    Pin versions for netlify

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ go install github.com/Vaelatern/mdbook-d2-go@latest
 export PATH="$PATH:~/go/bin"
 
 cargo install mdbook-callouts
+cargo install mdbook
 export PATH="$PATH:~/.cargo/bin"
 
 mdbook build
