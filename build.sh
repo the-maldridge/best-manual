0446f6a8262263ad51959c1b0b7ea785d5712ef8
Michael Aldridge <aldridge.mac@gmail.com>
2025-02-19 23:00:53 -0600

    build.sh: Further fix go path

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 go install github.com/Vaelatern/mdbook-d2-go@latest
-export PATH="$PATH:~/go/bin"
+export PATH="$PATH:/opt/buildhome/go/bin"
 
 cargo install mdbook-callouts
 cargo install mdbook
-export PATH="$PATH:~/.cargo/bin"
-
 mdbook build
